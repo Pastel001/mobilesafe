0481635acbd4354b0093790319ba3a9efb60970e
Pastel001 <daiwulong1989@126.com>
2015-11-14 20:49:34 +0800

    add update and insltall business

--- src/com/dwl/mobilesafe/SplashActivity.java
+++ src/com/dwl/mobilesafe/SplashActivity.java
@@ -1,33 +1,39 @@
 package com.dwl.mobilesafe;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
-import org.apache.http.HttpConnection;
+import net.tsz.afinal.FinalHttp;
+import net.tsz.afinal.http.AjaxCallBack;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.dwl.mobilesafe.utils.StreamTools;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.text.format.Time;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.dwl.mobilesafe.utils.StreamTools;
 
 public class SplashActivity extends Activity {
 	protected static final String TAG = "SplashActivity";
@@ -36,27 +42,39 @@ public class SplashActivity extends Activity {
 	protected static final int URL_ERROR = 3;
 	protected static final int NETWORK_ERROR = 4;
 	protected static final int JSON_ERROR = 5;
+	protected static final int UPDATE_INSTALL = 6;
 	private TextView tv_splash_version;
+	private TextView tv_splash_progress;
+	/**
+	 * 升级地址path
+	 */
+	private String path;
+	/**
+	 * 升级描述desc
+	 */
+	private String desc;
 	private Handler handler = new Handler() {
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
 			case SHOW_UPDATE_DIALOG:
-
+				showUpdateDialog();
 				break;
 			case ENTER_HOME:
 				enterHome();
 				break;
 			case URL_ERROR:
-				Toast.makeText(SplashActivity.this, "服务器路径错误。", 0).show();
+				Toast.makeText(SplashActivity.this, "服务器路径错误。",
+						Toast.LENGTH_SHORT).show();
 				enterHome();
 				break;
 			case NETWORK_ERROR:
-				Toast.makeText(getApplicationContext(), "网络连接错误。", 0).show();
+				Toast.makeText(getApplicationContext(), "网络连接错误。",
+						Toast.LENGTH_SHORT).show();
 				enterHome();
 				break;
 			case JSON_ERROR:
-				Toast.makeText(getApplicationContext(), "解析json数据错误。", 0)
-						.show();
+				Toast.makeText(getApplicationContext(), "解析json数据错误。",
+						Toast.LENGTH_SHORT).show();
 				enterHome();
 				break;
 			default:
@@ -70,11 +88,99 @@ public class SplashActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
 		tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
+		tv_splash_progress = (TextView) findViewById(R.id.tv_splash_progress);
 		tv_splash_version.setText("版本号：" + getVersion());
 		checkUpdate();
 		AlphaAnimation aa = new AlphaAnimation(0.3f, 1.0f);
 		aa.setDuration(2000);
 		findViewById(R.id.rl_splash_root).startAnimation(aa);
+	}
+
+	protected void showUpdateDialog() {
+		AlertDialog.Builder builder = new Builder(this);
+		// 设置dialog出现时按返回键进入首页
+		builder.setOnCancelListener(new OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				enterHome();
+			}
+		});
+		// dialog配置
+		builder.setTitle("有新版本啦！！！");
+		builder.setMessage(desc);
+		builder.setPositiveButton("立即更新", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// 下载apk
+				if (Environment.getExternalStorageState().equals(
+						Environment.MEDIA_MOUNTED)) {
+					FinalHttp fh = new FinalHttp();
+					fh.download(path, Environment.getExternalStorageDirectory()
+							.getAbsolutePath() + "/update.apk",
+							new AjaxCallBack<File>() {
+
+								@Override
+								public void onFailure(Throwable t, int errorNo,
+										String strMsg) {
+									super.onFailure(t, errorNo, strMsg);
+									Toast.makeText(getApplicationContext(),
+											"更新失败，请稍后重试！", Toast.LENGTH_SHORT)
+											.show();
+									enterHome();
+								}
+
+								@Override
+								public void onLoading(long count, long current) {
+									super.onLoading(count, current);
+									tv_splash_progress.setText("下载进度" + current
+											/ count * 100 + "%");
+								}
+
+								@Override
+								public void onSuccess(File t) {
+									super.onSuccess(t);
+									installApk(t);
+								}
+
+								private void installApk(File t) {
+									Intent intent = new Intent();
+									intent.setAction("android.intent.action.VIEW");
+									intent.addCategory("android.intent.category.DEFAULT");
+									intent.setDataAndType(Uri.fromFile(t),
+											"application/vnd.android.package-archive");
+									//startActivity(intent);
+									startActivityForResult(intent,UPDATE_INSTALL);
+								}
+							});
+				} else {
+					Toast.makeText(getApplicationContext(), "sdcard不可用",
+							Toast.LENGTH_SHORT).show();
+					enterHome();
+					return;
+				}
+
+			}
+		});
+		builder.setNegativeButton("下次再说", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				enterHome();
+			}
+		});
+		// dialog显示
+		builder.show();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Log.i(TAG, String.valueOf(resultCode));
+		//判断本地安装失败,安装失败进入首页
+		//系统安装页面关闭返回0，安装包解析错误1
+		if (requestCode == UPDATE_INSTALL && resultCode != 2) {
+			enterHome();
+		}
+		return;
 	}
 
 	protected void enterHome() {
@@ -87,7 +193,7 @@ public class SplashActivity extends Activity {
 		new Thread() {
 			@Override
 			public void run() {
-				long startTime = System.currentTimeMillis(); 
+				long startTime = System.currentTimeMillis();
 				Message msg = Message.obtain();
 				try {
 					URL url = new URL(getString(R.string.updateURL));
@@ -96,53 +202,52 @@ public class SplashActivity extends Activity {
 					conn.setReadTimeout(5000);
 					conn.setRequestMethod("GET");
 					int resultCode = conn.getResponseCode();
-					System.out.println(resultCode);
 					if (resultCode == 200) {
 						InputStream is = conn.getInputStream();
 						String result = StreamTools.readFromStream(is);
 						Log.i(TAG, result);
 						JSONObject obj = new JSONObject(result);
 						String version = (String) obj.get("version");
-						String desc = (String) obj.get("description");
-						String path = (String) obj.get("path");
-						if (Integer.parseInt(version) > Integer
-								.parseInt(getVersion())) {
+						desc = (String) obj.get("description");
+						path = (String) obj.get("path");
+						if (Double.parseDouble(version) > Double
+								.parseDouble(getVersion())) {
 							// 升级
 							msg.what = SHOW_UPDATE_DIALOG;
-							Log.i(TAG, "uodate");
+							Log.i(TAG, "update");
 						} else {
 							// 已经是新版本，不用升级
 							msg.what = ENTER_HOME;
-							Log.i(TAG, "dont uodate");
+							Log.i(TAG, "dont update");
 						}
 					}
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
 					msg.what = URL_ERROR;
-					Log.i(TAG, "dont uodate1");
 				} catch (IOException e) {
 					e.printStackTrace();
 					msg.what = NETWORK_ERROR;
-					Log.i(TAG, "dont uodate2");
+					Log.i(TAG, "dont update2");
 				} catch (JSONException e) {
 					e.printStackTrace();
 					msg.what = JSON_ERROR;
-					Log.i(TAG, "dont uodate3");
+					Log.i(TAG, "dont update3");
 				} finally {
 					long endTime = System.currentTimeMillis();
 					long dtime = endTime - startTime;
 					if (dtime < 2000) {
 						try {
-							sleep(2000-dtime);
+							sleep(2000 - dtime);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
 					handler.sendMessage(msg);
-					Log.i(TAG, "dont uodate,sendmsg");
+					Log.i(TAG, "sendmsg");
 				}
 			}
-		}.start();;
+		}.start();
+		;
 	}
 
 	/**
