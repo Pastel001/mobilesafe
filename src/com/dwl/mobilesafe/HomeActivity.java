694de63bcb6727f2c3615e6ae1010f4c304ca427
Pastel001 <daiwulong1989@126.com>
2015-11-15 18:59:56 +0800

    add HomeActivity basic layout

--- src/com/dwl/mobilesafe/HomeActivity.java
+++ src/com/dwl/mobilesafe/HomeActivity.java
@@ -2,11 +2,58 @@ package com.dwl.mobilesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
+	private GridView gv_home;
+	private static final String[] functionName = { "手机防盗", "通讯卫士", "软件管理",
+			"进程管理", "流量统计", "手机杀毒", "缓存清理", "高级工具", "设置中心" };
+	private static final int[] icon = { R.drawable.safe,
+			R.drawable.callmsgsafe, R.drawable.app, R.drawable.taskmanager,
+			R.drawable.netmanager, R.drawable.trojan, R.drawable.sysoptimize,
+			R.drawable.atools, R.drawable.settings };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		gv_home = (GridView) findViewById(R.id.gv_home);
+		gv_home.setAdapter(new HomeAdapter());
+	}
+
+	private class HomeAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return functionName.length;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = View.inflate(HomeActivity.this, R.layout.list_homepage_item, null);
+			ImageView iv_icon = (ImageView) v.findViewById(R.id.iv_home_item);
+			TextView tv_name = (TextView) v.findViewById(R.id.tv_home_item);
+			iv_icon.setImageResource(icon[position]);
+			tv_name.setText(functionName[position]);
+			return v;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
 	}
 }
