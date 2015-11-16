ab02f23d59eefb8260edc6644b020c19b58518d8
Pastel001 <daiwulong1989@126.com>
2015-11-17 00:19:03 +0800

    add attributes for my user-defined wiget SettingItemView

--- src/com/dwl/mobilesafe/SettingActivity.java
+++ src/com/dwl/mobilesafe/SettingActivity.java
@@ -3,6 +3,9 @@ package com.dwl.mobilesafe;
 import com.dwl.mobilesafe.ui.SettingItemView;
 
 import android.app.Activity;
+import android.app.backup.SharedPreferencesBackupHelper;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,22 +13,33 @@ import android.view.View.OnClickListener;
 public class SettingActivity extends Activity {
 	
 	private SettingItemView siv_update;
+	private SharedPreferences sp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		sp = getSharedPreferences("config", MODE_PRIVATE);
+		boolean update = sp.getBoolean("update", false);
 		setContentView(R.layout.activity_setting);
 		siv_update = (SettingItemView) findViewById(R.id.siv_update);
+		if (update) {
+			siv_update.setChecked(true);
+		} else {
+			siv_update.setChecked(false);
+		}
 		siv_update.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				Editor editor = sp.edit();
 				if(siv_update.isChecked()){
 					siv_update.setChecked(false);
-					siv_update.setDesc("自动更新已经关闭");
+					editor.putBoolean("update", false);
 				}else{
 					siv_update.setChecked(true);
-					siv_update.setDesc("自动更新已经开启");
+					editor.putBoolean("update", true);
 				}
+				editor.commit();
 			}
 		});
 	}
--- src/com/dwl/mobilesafe/ui/SettingItemView.java
+++ src/com/dwl/mobilesafe/ui/SettingItemView.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 /**
  * 做一个特殊的相对布局。 一出生 布局里面显示的是2个textview 一个checkbox 还有一个view
  */
@@ -16,8 +17,12 @@ public class SettingItemView extends RelativeLayout {
 	private TextView tv_desc;
 	private CheckBox cb_status;
 
+	private String title;
+	private String desc_on;
+	private String desc_off;
+
 	private void initView(Context context) {
-		//转化布局文件->view对象 这个view对象直接挂载在自己身上。
+		// 转化布局文件->view对象 这个view对象直接挂载在自己身上。
 		View.inflate(context, R.layout.ui_setting_item_view, this);
 		tv_title = (TextView) this.findViewById(R.id.tv_title);
 		tv_desc = (TextView) this.findViewById(R.id.tv_desc);
@@ -32,6 +37,18 @@ public class SettingItemView extends RelativeLayout {
 	public SettingItemView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		initView(context);
+		title = attrs.getAttributeValue(
+				"http://schemas.android.com/apk/res/com.dwl.mobilesafe",
+				"title");
+		desc_on = attrs.getAttributeValue(
+				"http://schemas.android.com/apk/res/com.dwl.mobilesafe",
+				"desc_on");
+		desc_off = attrs.getAttributeValue(
+				"http://schemas.android.com/apk/res/com.dwl.mobilesafe",
+				"desc_off");
+		tv_title.setText(title);
+		tv_desc.setText(desc_off);
+
 	}
 
 	public SettingItemView(Context context) {
@@ -42,14 +59,21 @@ public class SettingItemView extends RelativeLayout {
 	public boolean isChecked() {
 		return cb_status.isChecked();
 	}
-	public void setChecked(boolean checked){
+
+	public void setChecked(boolean checked) {
 		cb_status.setChecked(checked);
+		if (isChecked()) {
+			tv_desc.setText(desc_on);
+		} else {
+			tv_desc.setText(desc_off);
+		}
 	}
-	
-	public void setTitle(String text){
+
+	public void setTitle(String text) {
 		tv_title.setText(text);
 	}
-	public void setDesc(String text){
+
+	public void setDesc(String text) {
 		tv_desc.setText(text);
 	}
 
